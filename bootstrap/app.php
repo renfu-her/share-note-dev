9d2b2aec2ea6039dacb6c75f3e96a30fe100b4a6
Patrick Her <renfu.her@gmail.com>
2024-11-08 08:50:23 +0800

    Refactor app.php and add AdminAuthMiddleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AdminAuthMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // 使用 alias 方法為中間件設置別名
+        $middleware->alias([
+            'admin.auth' => AdminAuthMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
